0bd3ab0b08c5bb8600c9e5146ae46bc9e32507de
Rasel Shahriar <raselhossain.rafi@gmail.com>
2022-09-17 18:50:19 +0600

    debugg creat slider worked

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ const getImages = (query) => {
   fetch(`https://pixabay.com/api/?key=${KEY}=${query}&image_type=photo&pretty=true`)
     .then(response => response.json())
     .then(data => {
-      console.log(data.hits);
+      console.log(data.hits)
       showImages(data.hits)
     })
     .catch(err => console.log(err))
